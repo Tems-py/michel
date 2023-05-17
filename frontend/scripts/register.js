aa0e429d7b267fcb29db325a3ea8d73609b0850d
Krzychu <redshadowspy1@wp.pl>
2023-05-17 12:30:08 +0200

    Login and register js requests

---
+++ frontend/scripts/register.js
@@ -0,0 +1,15 @@
+document.getElementById("register").addEventListener("click", () => {
+    axios.get('https://jsonplaceholder.typicode.com/posts/1')
+    .then(function (response) {
+        console.log(response);
+        popup(response.data['title'])
+    })
+})
+
+function popup(text){
+    document.getElementById("popup").innerHTML = text
+    document.getElementById("popup").style.transform = "translateX(0%)"
+    setTimeout(() => {
+        document.getElementById("popup").style.transform = "translateX(300%)"
+    }, 2000)
+}
