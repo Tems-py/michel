1e54c21e4025420f272dff7fd89805cb8041fdee
Tems <tems@tems.pl>
2023-05-17 21:43:54 +0200

    Added course list

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,37 @@
-from flask import Flask
-
+from flask import Flask, jsonify
+from flask_restful import Resource, Api
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
+api = Api(app)
+
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'michel'
+
+mysql = MySQL()
+mysql.init_app(app)
 
 
-@app.route("/")
-def index():
-    return "N"
+class Course(Resource):
+    def get(self):
+        cursor = mysql.get_db().cursor()
+        cursor.execute("SELECT id, category, subcategory FROM course")
+        l = []
+        for x in cursor.fetchall():
+            l.append({'id': x[0], 'category': x[1], 'subcategory': x[2]})
+
+        return jsonify(l)
+
+
+class Pages(Resource):
+    def get(self, id_page):
+        return jsonify(id_page)
+
+
+api.add_resource(Course, '/course')
+api.add_resource(Pages, '/course/<int:id_page>')
 
 if __name__ == "__main__":
     app.run()
