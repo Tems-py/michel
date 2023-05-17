d43bb749336d17af0e8f5712c4a7b5ed18077e22
Krzychu <redshadowspy1@wp.pl>
2023-05-17 11:43:36 +0200

    Navbar logout and redirects

--- frontend/scripts/auth.js
+++ frontend/scripts/auth.js
@@ -1,12 +1,14 @@
 function checkLogin(){
     if (localStorage.getItem("token")){
         let hide = document.getElementsByClassName("hide_when_logged");
-        for (let x in hide.length){
+        console.log(hide.length)
+        for (let x=0;x < hide.length; x++){
+            console.log(hide[x])
             hide[x].classList.add("hide")
         }
         let show = document.getElementsByClassName("show_when_logged");
-        for (let x in show.length){
-            hide[x].classList.remove("hide")
+        for (let x=0; x < show.length; x++){
+            show[x].classList.remove("hide")
         }
     }
 }
