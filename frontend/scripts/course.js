6518dbf6e6c35662e037e0ee0e0eedaff73b81b5
tems <tems@tems.pl>
2023-05-22 14:29:11 +0200

    KOcham ciepłe zadki i bary

--- frontend/scripts/course.js
+++ frontend/scripts/course.js
@@ -1,17 +1,66 @@
 let d ;
 let x = true;
+let course_lenght = 0;
 axios.get('https://michel.temss.tech/course')
 .then(function (response) {
-  a = response.data.length;
-  d = response.data;
-  z = "topic_";
-  for(b=0; b<a; b++){
-    c = z + String(b+1);
-    document.getElementById("topic").innerHTML += "<li>" + d[b]["subcategory"] + "</li>";
+  a = response.data;
+  course_lenght = a.length;
+  for (let i=0; i<a.length; i++){
+    let li = document.createElement("li");
+    li.classList.add("sidebar")
+    li.innerHTML = a[i]['subcategory'];
+
+    li.addEventListener("click", () => {
+      axios.get(`https://michel.temss.tech/course/${a[i]['id']}`)
+        .then(function (response) {
+          data = response.data;
+          console.log(a[i])
+          document.getElementById("content").innerHTML = data['content']
+          document.getElementById("title").innerHTML = data['subcategory']
+          side_menu()
+          let buttons = document.getElementsByClassName("sidebar")
+          for (let i=0; i<buttons.length; i++){
+            buttons[i].classList.remove('highlight')
+          }
+          li.classList.add("highlight")
+          current_page = i
+          update_bar()
+      })
+    })
+
+    document.getElementById("topic").appendChild(li);
   }
-    document.getElementById("title").innerHTML = response.data[0]["subcategory"];
-    document.getElementById("content").innerHTML = response.data[0]["category"];
-  })
+  change()
+  update_bar()
+})
+
+function update_bar(){
+  let progr = document.getElementById("progress2")
+  document.getElementById("progr").innerHTML = `${Math.round(current_page/(course_lenght-1) * 100)}%`
+  progr.style.transform =  `scaleX(${(current_page/(course_lenght-1))})`
+}
+
+let current_page = 0
+function change(){
+  let buttons = document.getElementsByClassName("sidebar")
+  update_bar()
+  buttons[current_page].click()
+  side_menu()
+}
+
+document.getElementById("prev").addEventListener("click", () => {
+  if (current_page > 0){
+    current_page -= 1;
+    change()
+  }
+})
+
+document.getElementById("next").addEventListener("click", () => {
+  if (current_page < course_lenght - 1){
+    current_page += 1;
+    change()
+  }
+})
 
 x = true;
 function side_menu(){
