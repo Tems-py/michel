07c64df9a4237d5c152a1deac98da051c0c1ba08
iHateNigeria <jusebhjusebh@gmail.com>
2023-06-02 13:18:09 +0200

    llol

--- frontend/scripts/course.js
+++ frontend/scripts/course.js
@@ -1,4 +1,5 @@
 let d ;
+let x = true;
 axios.get('https://michel.temss.tech/course')
 .then(function (response) {
   a = response.data.length;
@@ -8,24 +9,19 @@ axios.get('https://michel.temss.tech/course')
     c = z + String(b+1);
     document.getElementById("topic").innerHTML += "<li>" + d[b]["subcategory"] + "</li>";
   }
-    console.log(j);
-    document.getElementById("title").innerHTML = response.data[j]["subcategory"];
-    // for(i=0; i<8; i++){
-    //   y = z + String(i+1);
-    //   console.log(y)
-    //   document.getElementById(y).innerHTML = response.data[i]["subcategory"];
-    //   y = "";
-    // }
+    document.getElementById("title").innerHTML = response.data[0]["subcategory"];
+    document.getElementById("content").innerHTML = response.data[0]["category"];
   })
+
 x = true;
 function side_menu(){
-    if(x){
-        x = false;
-        document.getElementById("menu_content").style.transform = "translateX(300px)";
-    }
-    else{
-        x = true;
-        document.getElementById("menu_content").style.transform = "translateX(-300px)";
-    }
+  if(x == true){
+    x = false;
+    document.getElementById("menu_content").style.transform = "translateX(300px)";
+  }
+  else{
+    x = true;
+    document.getElementById("menu_content").style.transform = "translateX(-300px)";
+  }
 }
   
